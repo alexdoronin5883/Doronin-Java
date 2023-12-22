50d4f018b967dcb1947b65def7f4b256dc2bd098
Александр Доронин <107746307+alexdoronin5883@users.noreply.github.com>
2023-12-22 18:23:15 +0300

    Update Task_3.java

--- src/Task_3.java
+++ src/Task_3.java
@@ -2,6 +2,8 @@ import java.util.Arrays;
 import java.util.Scanner;
 import static java.lang.System.*;
 
+//Составить алгоритм: на входе есть числовой массив, необходимо вывести элементы массива кратные 3
+
 public class Task_3 {
     public static void main(String[] args) {
         int[] arr = new int[5];
