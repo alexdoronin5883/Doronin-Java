242ed17b7d1b145622de0c078bac86e8f4cc8f0e
Александр Доронин <107746307+alexdoronin5883@users.noreply.github.com>
2023-12-22 18:22:08 +0300

    Update Task_2.java

--- src/Task_2.java
+++ src/Task_2.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 import static java.lang.System.*;
 
+//Составить алгоритм: если введенное имя совпадает с Вячеслав, 
+//то вывести “Привет, Вячеслав”, если нет, то вывести "Нет такого имени"
+
 public class Task_2 {
     public static void main(String[] args) {
         Scanner cn = new Scanner(System.in);
